9cbb9964a9f128b42cd555496cd7b46cf8498d35
TravisLovingood <61667953+TravisLovingood@users.noreply.github.com>
2020-05-08 15:33:06 -0400

    Added Inquirer and prompts

--- index.js
+++ index.js
@@ -1,35 +1,58 @@
-const generateReadme = require("generate-readme");
-const fs = require("fs");
+const inquirer = require("inquirer");
+;
 
-
-
-    
-function promptuser(){
-
-
-
-} 
-
-
-
-
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "Project Title",
+      message: "What is your project title?"
+    },
+    {
+      type: "input",
+      name: "Project Description",
+      message: "What is your project name?"
+    },
+    {
+      type: "inpu0t",
+      name: "Table of contents",
+      message: "Add a table of contents"
+    },
+    {
+      type: "input",
+      name: "Contributors",
+      message: "List Contributors?"
+    }
+    {
+      type: "input",
+      name: "License",
+      message: "License?"
+    }
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username"
+    }
+    {
+      type: "input",
+      name: "Email",
+      message: "Enter yuor Github Email?"
+    }      
+  ]); 
+}
 
 
 function generateReadme(answers) {
 
 
-
-
-
+    
 }
-
-
 
 promptuser()
     .then(function(answers) {
-        const html = generateReadMe(awnsers)
+        const readme = generateReadMe(awnsers)
                         // Need to fill in where the return goes!!!
-        return 
+        return writeReadMe(readme)
     })
     .then(function() {
         console.log("Success!");    
